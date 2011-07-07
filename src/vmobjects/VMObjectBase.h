b1bc69f2fb827f377ace5a60eb6459d8e6b1f00a
christian@21b74f68-547d-474f-912b-30106d561002 <christian@21b74f68-547d-474f-912b-30106d561002>
2011-07-07 15:29:16 +0000

    forgot to add file in previous checkin
    --HG--
    extra : convert_revision : svn%3A21b74f68-547d-474f-912b-30106d561002/som%2B%2B%4057

---
+++ src/vmobjects/VMObjectBase.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define MASK_OBJECT_IS_MARKED (1 << 0)
+#define MASK_OBJECT_IS_OLD (1 << 1)
+#define MASK_SEEN_BY_WRITE_BARRIER (1 << 2)
+
+class VMObjectBase {
+protected:
+	int32_t gcfield;
+public:
+	int32_t GetGCField() const;
+	void SetGCField(int32_t);
+};
+
+inline int32_t VMObjectBase::GetGCField() const {
+    return gcfield;
+}
+inline void VMObjectBase::SetGCField(int32_t val) {
+    gcfield = val;
+}
+
