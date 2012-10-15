5c2221a006ff7de45747996689bedb44eca85a16
Christian Boland <boland1983@gmail.com>
2012-10-15 08:28:32 +0200

    generate images to subfolder generated_images

--- scripts/create_plots.py
+++ scripts/create_plots.py
@@ -3,6 +3,9 @@ from os import listdir
 from os import rename
 from subprocess import call
 import argparse
+import shutil
+
+OUT_DIR = "generated_images/"
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Run Benchmarks for all SOM++ VMs')
@@ -12,10 +15,14 @@ if __name__=="__main__":
     files=[s for s in listdir("scripts") if s.endswith(".gp")]
     for gp_file in files:
         src_file = "scripts/" + gp_file
-        eps_file = gp_file[:gp_file.rfind(".")]+".eps"
-        png_file = gp_file[:gp_file.rfind(".")]+".png"
+        eps_file = OUT_DIR + gp_file[:gp_file.rfind(".")]+".eps"
+        png_file = OUT_DIR + gp_file[:gp_file.rfind(".")]+".png"
         print "creating %s from %s" % (" and ".join(args.output_types), src_file)
         if "eps" in args.output_types:
             call(["gnuplot", src_file])
+            shutil.move(gp_file[:gp_file.rfind(".")]+".eps", eps_file)
         if "png" in args.output_types:
             call(["convert", "-density", "300", eps_file, png_file])
+        if "pdf" in args.output_types:
+            print "epstopdf %s" % eps_file
+            call(["epstopdf %s" % eps_file], shell=True)
