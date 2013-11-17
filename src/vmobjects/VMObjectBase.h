8011cc99d454e72726c4f230cf3660754908b1d4
Stefan Marr <git@stefan-marr.de>
2013-11-17 16:16:47 +0100

    Added assert to make sure forward pointers are not lost during GC
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/vmobjects/VMObjectBase.h
+++ src/vmobjects/VMObjectBase.h
@@ -6,6 +6,7 @@
 #define MASK_OBJECT_IS_MARKED (1 << 0)
 #define MASK_OBJECT_IS_OLD (1 << 1)
 #define MASK_SEEN_BY_WRITE_BARRIER (1 << 2)
+#define MASK_BITS_ALL (MASK_OBJECT_IS_MARKED | MASK_OBJECT_IS_OLD | MASK_SEEN_BY_WRITE_BARRIER)
 
 class VMObjectBase {
 protected:
@@ -19,6 +20,12 @@ size_t VMObjectBase::GetGCField() const {
     return gcfield;
 }
 void VMObjectBase::SetGCField(size_t val) {
+    // if gcfield is used as a forwarding pointer it should not be overwritten
+    // with simple mark bits, because the object itself is garbage but the
+    // forwarding address needs to be maintained incase any object still points
+    // to the garbage object.
+    #define GCFIELD_IS_NOT_FORWARDING_POINTER (gcfield <= MASK_BITS_ALL)
+    assert(GCFIELD_IS_NOT_FORWARDING_POINTER || val > MASK_BITS_ALL);
     gcfield = val;
 }
 
