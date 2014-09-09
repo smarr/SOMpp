484eeeceafa0374489945ebb9273e8d5f69b31e4
Stefan Marr <git@stefan-marr.de>
2014-09-09 15:17:32 +0200

    Fix whitespace
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/interpreter/Interpreter.cpp
+++ src/interpreter/Interpreter.cpp
@@ -78,108 +78,125 @@ void Interpreter::Start() {
     method = GetFrame()->GetMethod();
     currentBytecodes = method->GetBytecodes();
 
-void* loopTargets[] = {
-    &&LABEL_BC_HALT,
-    &&LABEL_BC_DUP,
-    &&LABEL_BC_PUSH_LOCAL,
-    &&LABEL_BC_PUSH_ARGUMENT,
-    &&LABEL_BC_PUSH_FIELD,
-    &&LABEL_BC_PUSH_BLOCK,
-    &&LABEL_BC_PUSH_CONSTANT,
-    &&LABEL_BC_PUSH_GLOBAL,
-    &&LABEL_BC_POP,
-    &&LABEL_BC_POP_LOCAL,
-    &&LABEL_BC_POP_ARGUMENT,
-    &&LABEL_BC_POP_FIELD,
-    &&LABEL_BC_SEND,
-    &&LABEL_BC_SUPER_SEND,
-    &&LABEL_BC_RETURN_LOCAL,
-    &&LABEL_BC_RETURN_NON_LOCAL,
-    &&LABEL_BC_JUMP_IF_FALSE,
-    &&LABEL_BC_JUMP_IF_TRUE,
-    &&LABEL_BC_JUMP
-}
-;
+    void* loopTargets[] = {
+        &&LABEL_BC_HALT,
+        &&LABEL_BC_DUP,
+        &&LABEL_BC_PUSH_LOCAL,
+        &&LABEL_BC_PUSH_ARGUMENT,
+        &&LABEL_BC_PUSH_FIELD,
+        &&LABEL_BC_PUSH_BLOCK,
+        &&LABEL_BC_PUSH_CONSTANT,
+        &&LABEL_BC_PUSH_GLOBAL,
+        &&LABEL_BC_POP,
+        &&LABEL_BC_POP_LOCAL,
+        &&LABEL_BC_POP_ARGUMENT,
+        &&LABEL_BC_POP_FIELD,
+        &&LABEL_BC_SEND,
+        &&LABEL_BC_SUPER_SEND,
+        &&LABEL_BC_RETURN_LOCAL,
+        &&LABEL_BC_RETURN_NON_LOCAL,
+        &&LABEL_BC_JUMP_IF_FALSE,
+        &&LABEL_BC_JUMP_IF_TRUE,
+        &&LABEL_BC_JUMP
+    };
 
-goto *loopTargets[currentBytecodes[bytecodeIndexGlobal]];
+    goto *loopTargets[currentBytecodes[bytecodeIndexGlobal]];
 
-//
-// THIS IS THE former interpretation loop
-LABEL_BC_HALT:
-  PROLOGUE(1);
-return; // handle the halt bytecode
-LABEL_BC_DUP: 
-  PROLOGUE(1);
-doDup();
-  DISPATCH_NOGC();
-LABEL_BC_PUSH_LOCAL:       
-  PROLOGUE(3);
-doPushLocal(bytecodeIndexGlobal - 3);
-  DISPATCH_NOGC();
-LABEL_BC_PUSH_ARGUMENT:
-  PROLOGUE(3);
-doPushArgument(bytecodeIndexGlobal - 3);
-  DISPATCH_NOGC();
-LABEL_BC_PUSH_FIELD:
-  PROLOGUE(2);
-doPushField(bytecodeIndexGlobal - 2);
-  DISPATCH_NOGC();
-LABEL_BC_PUSH_BLOCK:
-  PROLOGUE(2);
-doPushBlock(bytecodeIndexGlobal - 2);
-  DISPATCH_GC();
-LABEL_BC_PUSH_CONSTANT:
-  PROLOGUE(2);
-doPushConstant(bytecodeIndexGlobal - 2);
-  DISPATCH_NOGC();
-LABEL_BC_PUSH_GLOBAL:
-  PROLOGUE(2);
-doPushGlobal(bytecodeIndexGlobal - 2);
-  DISPATCH_GC();
-LABEL_BC_POP:
-  PROLOGUE(1);
-doPop();
-  DISPATCH_NOGC();
-LABEL_BC_POP_LOCAL:
-  PROLOGUE(3);
-doPopLocal(bytecodeIndexGlobal - 3);
-  DISPATCH_NOGC();
-LABEL_BC_POP_ARGUMENT:
-  PROLOGUE(3);
-doPopArgument(bytecodeIndexGlobal - 3);
-  DISPATCH_NOGC();
-LABEL_BC_POP_FIELD:
-  PROLOGUE(2);
-doPopField(bytecodeIndexGlobal - 2);
-  DISPATCH_NOGC();
-LABEL_BC_SEND:
-  PROLOGUE(2);
-doSend(bytecodeIndexGlobal - 2);
-  DISPATCH_GC();
-LABEL_BC_SUPER_SEND:
-  PROLOGUE(2);
-doSuperSend(bytecodeIndexGlobal - 2);
-  DISPATCH_GC();
-LABEL_BC_RETURN_LOCAL:
-  PROLOGUE(1);
-doReturnLocal();
-  DISPATCH_NOGC();
-LABEL_BC_RETURN_NON_LOCAL:
-  PROLOGUE(1);
-doReturnNonLocal();
-  DISPATCH_NOGC();
-LABEL_BC_JUMP_IF_FALSE:
-  PROLOGUE(5);
-doJumpIfFalse(bytecodeIndexGlobal - 5);
-  DISPATCH_NOGC();
-LABEL_BC_JUMP_IF_TRUE:
-  PROLOGUE(5);
-doJumpIfTrue(bytecodeIndexGlobal - 5);
-  DISPATCH_NOGC();
-LABEL_BC_JUMP:
-  PROLOGUE(5);
-doJump(bytecodeIndexGlobal - 5);
-DISPATCH_NOGC();
+    //
+    // THIS IS THE former interpretation loop
+    LABEL_BC_HALT:
+      PROLOGUE(1);
+      return; // handle the halt bytecode
+
+    LABEL_BC_DUP:
+      PROLOGUE(1);
+      doDup();
+      DISPATCH_NOGC();
+
+    LABEL_BC_PUSH_LOCAL:       
+      PROLOGUE(3);
+      doPushLocal(bytecodeIndexGlobal - 3);
+      DISPATCH_NOGC();
+
+    LABEL_BC_PUSH_ARGUMENT:
+      PROLOGUE(3);
+      doPushArgument(bytecodeIndexGlobal - 3);
+      DISPATCH_NOGC();
+
+    LABEL_BC_PUSH_FIELD:
+      PROLOGUE(2);
+      doPushField(bytecodeIndexGlobal - 2);
+      DISPATCH_NOGC();
+
+    LABEL_BC_PUSH_BLOCK:
+      PROLOGUE(2);
+      doPushBlock(bytecodeIndexGlobal - 2);
+      DISPATCH_GC();
+
+    LABEL_BC_PUSH_CONSTANT:
+      PROLOGUE(2);
+      doPushConstant(bytecodeIndexGlobal - 2);
+      DISPATCH_NOGC();
+
+    LABEL_BC_PUSH_GLOBAL:
+      PROLOGUE(2);
+      doPushGlobal(bytecodeIndexGlobal - 2);
+      DISPATCH_GC();
+
+    LABEL_BC_POP:
+      PROLOGUE(1);
+      doPop();
+      DISPATCH_NOGC();
+
+    LABEL_BC_POP_LOCAL:
+      PROLOGUE(3);
+      doPopLocal(bytecodeIndexGlobal - 3);
+      DISPATCH_NOGC();
+
+    LABEL_BC_POP_ARGUMENT:
+      PROLOGUE(3);
+      doPopArgument(bytecodeIndexGlobal - 3);
+      DISPATCH_NOGC();
+
+    LABEL_BC_POP_FIELD:
+      PROLOGUE(2);
+      doPopField(bytecodeIndexGlobal - 2);
+      DISPATCH_NOGC();
+
+    LABEL_BC_SEND:
+      PROLOGUE(2);
+      doSend(bytecodeIndexGlobal - 2);
+      DISPATCH_GC();
+
+    LABEL_BC_SUPER_SEND:
+      PROLOGUE(2);
+      doSuperSend(bytecodeIndexGlobal - 2);
+      DISPATCH_GC();
+
+    LABEL_BC_RETURN_LOCAL:
+      PROLOGUE(1);
+      doReturnLocal();
+      DISPATCH_NOGC();
+
+    LABEL_BC_RETURN_NON_LOCAL:
+      PROLOGUE(1);
+      doReturnNonLocal();
+      DISPATCH_NOGC();
+
+    LABEL_BC_JUMP_IF_FALSE:
+      PROLOGUE(5);
+      doJumpIfFalse(bytecodeIndexGlobal - 5);
+      DISPATCH_NOGC();
+
+    LABEL_BC_JUMP_IF_TRUE:
+      PROLOGUE(5);
+      doJumpIfTrue(bytecodeIndexGlobal - 5);
+      DISPATCH_NOGC();
+
+    LABEL_BC_JUMP:
+      PROLOGUE(5);
+      doJump(bytecodeIndexGlobal - 5);
+      DISPATCH_NOGC();
 }
 
 pVMFrame Interpreter::PushNewFrame(pVMMethod method) {
@@ -201,7 +218,7 @@ void Interpreter::SetFrame(pVMFrame frame) {
 
 oop_t Interpreter::GetSelf() const {
     pVMFrame context = GetFrame()->GetOuterContext();
-    return context->GetArgument(0,0);
+    return context->GetArgument(0, 0);
 }
 
 pVMFrame Interpreter::popFrame() {
