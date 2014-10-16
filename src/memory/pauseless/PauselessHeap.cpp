2ebe84864d28284f993bbd9acb1a793404925106
Jeroen De Geeter <jeroen.degeeter@gmail.com>
2014-10-16 15:14:14 +0200

    - Changed the FLIP_NMT_VALUE macro - Added template methods for read barriers that can be called from either gc threads or mutator threads.

--- src/memory/pauseless/PauselessHeap.cpp
+++ src/memory/pauseless/PauselessHeap.cpp
@@ -1,11 +1,13 @@
 #include "PauselessHeap.h"
 #include "../Page.h"
-#include "PauselessCollector.h"
+//#include "PauselessCollector.h"
 #include "../../vmobjects/AbstractObject.h"
 #include "../../vm/Universe.h"
+#include "PauselessCollectorThread.h"
 
 #include <string.h>
 #include <iostream>
+#include <pthread.h>
 
 #include <sys/mman.h>
 
@@ -13,43 +15,50 @@
 
 #define NUMBER_OF_GC_THREADS 4
 
-PauselessHeap::PauselessHeap(long objectSpaceSize, long pageSize) {
-    gc = new PauselessCollector(this, NUMBER_OF_GC_THREADS);
+PauselessHeap::PauselessHeap(long objectSpaceSize, long pageSize) : PagedHeap(objectSpaceSize, pageSize) {
+    //gc = new PauselessCollector(this, NUMBER_OF_GC_THREADS); //perhaps I should change this?
+    pthread_key_create(&pauselessCollectorThread, NULL);
 }
 
 void PauselessHeap::SignalRootSetMarked() {
-    static_cast<PauselessCollector*>(gc)->SignalRootSetMarked();
+    PauselessCollectorThread::SignalRootSetMarked();
+    //static_cast<PauselessCollector*>(gc)->SignalRootSetMarked();
 }
 
 void PauselessHeap::SignalInterpreterBlocked(Interpreter* interpreter) {
-    static_cast<PauselessCollector*>(gc)->AddBlockedInterpreter(interpreter);
+    PauselessCollectorThread::AddBlockedInterpreter(interpreter);
+    //static_cast<PauselessCollector*>(gc)->AddBlockedInterpreter(interpreter);
 }
 
 void PauselessHeap::SignalSafepointReached() {
-    static_cast<PauselessCollector*>(gc)->SignalSafepointReached();
+    PauselessCollectorThread::SignalSafepointReached();
+    //static_cast<PauselessCollector*>(gc)->SignalSafepointReached();
+}
+
+void PauselessHeap::SignalGCTrapEnabled() {
+    pthread_cond_signal(&gcTrapEnabledCondition);
+}
+
+void PauselessHeap::Start() {
+    for (int i=0; i < NUMBER_OF_GC_THREADS; i++) {
+        pthread_t tid = 0;
+        pthread_create(&tid, NULL, &ThreadForGC, NULL);
+    }
+}
+
+void* PauselessHeap::ThreadForGC(void*) {
+    PauselessCollectorThread* gcThread = new PauselessCollectorThread();
+    _HEAP->AddGCThread(gcThread);
+    gcThread->Collect();
+    return nullptr;
+}
+
+PauselessCollectorThread* PauselessHeap::GetGCThread() {
+    return (PauselessCollectorThread*)pthread_getspecific(pauselessCollectorThread);
+}
+
+void PauselessHeap::AddGCThread(PauselessCollectorThread* gcThread) {
+    pthread_setspecific(pauselessCollectorThread, gcThread);
 }
 
 #endif
-
-
-
-/*
- Page* PauselessHeap::GCRequestPage() {
- Page* newPage;
- pthread_mutex_lock(&pageRequestMutex);
- if (availablePages->empty()) {
- newPage = new Page(nextFreePagePosition, this);
- assert(newPage);
- allPages->push_back(newPage);
- nextFreePagePosition = (void*) ((size_t)nextFreePagePosition + pageSize);
- checkCollectionTreshold();
- //if (nextFreePagePosition > collectionLimit) triggerGC();
- } else {
- newPage = availablePages->back();
- availablePages->pop_back();
- }
- pthread_mutex_unlock(&pageRequestMutex);
- return newPage;
- }
- 
- */
--- src/memory/pauseless/PauselessHeap.h
+++ src/memory/pauseless/PauselessHeap.h
@@ -7,67 +7,154 @@
 
 #include "../../interpreter/Interpreter.h"
 #include <memory/PagedHeap.h>
-#include <memory/Page.h>§
+#include <memory/Page.h>
 #include "../../vmobjects/VMObjectBase.h"
 #include "../../vmobjects/AbstractObject.h"
 #include "../../vm/Universe.h"
+#include "PauselessCollectorThread.h"
 
 #define MASK_OBJECT_NMT (1 << 1)
-#define REFERENCE_NMT_VALUE(REFERENCE) (((reinterpret_cast<uintptr_t>(REFERENCE) & MASK_OBJECT_NMT) == 0) ? false : true)
-#define FLIP_NMT_VALUE(REFERENCE) (((reinterpret_cast<intptr_t>(REFERENCE) & MASK_OBJECT_NMT) == 0) ? ((pVMObject)(reinterpret_cast<intptr_t>(REFERENCE) | MASK_OBJECT_NMT)) : ((pVMObject)(reinterpret_cast<intptr_t>(REFERENCE) ^ MASK_OBJECT_NMT)))
+#define REFERENCE_NMT_VALUE(REFERENCE) (((reinterpret_cast<intptr_t>(REFERENCE) & MASK_OBJECT_NMT) == 0) ? false : true)
+#define FLIP_NMT_VALUE(REFERENCE) ( (reinterpret_cast<intptr_t>(REFERENCE) ^ MASK_OBJECT_NMT) )
 
 class PauselessHeap : public PagedHeap {
  
 public:
-    PauselessHeap(long objectSpaceSize = HEAP_SIZE, long pageSize = PAGE_SIZE);
-    
-    void ReadBarrier(void** referenceHolder);
-    void NMTTrap(void**);
-    void GCTrap(void**);
-    
+    PauselessHeap(long, long);
     
     void SignalRootSetMarked();
     void SignalInterpreterBlocked(Interpreter*);
     void SignalSafepointReached();
+    void SignalGCTrapEnabled();
+    
+    void Start();
+    
+    PauselessCollectorThread* GetGCThread();
+    void AddGCThread(PauselessCollectorThread*);
+    
+private:
+    
+    pthread_key_t pauselessCollectorThread;
+    pthread_mutex_t gcTrapEnabledMutex;
+    pthread_cond_t gcTrapEnabledCondition;
+    
+    static void* ThreadForGC(void*);
     
 };
 
-inline void PauselessHeap::NMTTrap(void** referenceHolder) {
-    if (_UNIVERSE->GetInterpreter()->GetExpectedNMT() != REFERENCE_NMT_VALUE(*referenceHolder)) {
-        *referenceHolder = FLIP_NMT_VALUE(*referenceHolder);
-        _UNIVERSE->GetInterpreter()->AddGCWork((AbstractVMObject*)*referenceHolder);
+template<typename T>
+inline T* FLIP(T* reference) {
+    return (T*) FLIP_NMT_VALUE(reference);
+}
+
+template<typename T>
+inline T* WriteBarrier(T* reference) {
+    if (_UNIVERSE->GetInterpreter()->GetExpectedNMT())
+        return (T*) FLIP_NMT_VALUE(reference);
+    else
+        return reference;
+}
+
+template<typename T>
+inline T* WriteBarrierForGCThread(T* reference) {
+    if (_HEAP->GetGCThread()->GetExpectedNMT())
+        return (T*) FLIP_NMT_VALUE(reference);
+    else
+        return reference;
+}
+
+template<typename T>
+inline T* Untag(T* reference) {
+    if (REFERENCE_NMT_VALUE(reference))
+        return (T*)FLIP_NMT_VALUE(reference);
+    else
+        return reference;
+}
+
+template<typename T>
+inline void NMTTrap(T** referenceHolder) {
+    Interpreter* interpreter = _UNIVERSE->GetInterpreter();
+    if (interpreter->GetExpectedNMT() != REFERENCE_NMT_VALUE(*referenceHolder)) {
+        interpreter->AddGCWork((AbstractVMObject*)*referenceHolder);
+        *referenceHolder = (T*)FLIP_NMT_VALUE(*referenceHolder); //COMPARE AND SWAP NEEDED
     }
 }
 
-inline void PauselessHeap::GCTrap(void** referenceHolder) {
+template<typename T>
+inline void NMTTrapForGCThread(T** referenceHolder) {
+    PauselessCollectorThread* gcThread = _HEAP->GetGCThread();
+    if (gcThread->GetExpectedNMT() != REFERENCE_NMT_VALUE(*referenceHolder)) {
+        gcThread->AddGCWork((AbstractVMObject*)*referenceHolder);
+        *referenceHolder = (T*)FLIP_NMT_VALUE(*referenceHolder); //COMPARE AND SWAP NEEDED
+    }
+}
+
+template<typename T>
+inline T* ReadBarrier(T** referenceHolder) {
+    if (*referenceHolder == nullptr)
+        return (T*)nullptr;
+    NMTTrap(referenceHolder);
+    return Untag(*referenceHolder);
+}
+
+template<typename T>
+inline T* ReadBarrierForGCThread(T** referenceHolder) {
+    if (*referenceHolder == nullptr)
+        return (T*)nullptr;
+    //GCTrapGC(referenceHolder);
+    NMTTrapForGCThread(referenceHolder);
+    return Untag(*referenceHolder);
+}
+
+/*
+inline void NMTTrap(void** referenceHolder) {
+    Interpreter* interpreter = _UNIVERSE->GetInterpreter();
+    if (interpreter->GetExpectedNMT() != REFERENCE_NMT_VALUE(*referenceHolder)) {
+        cout << "NMT trap activated" << endl;
+        interpreter->AddGCWork((AbstractVMObject*)*referenceHolder);
+        *referenceHolder = FLIP_NMT_VALUE(*referenceHolder); //COMPARE AND SWAP NEEDED
+    }
+}
+
+inline AbstractVMObject* Untag(void* reference) {
+    if (REFERENCE_NMT_VALUE(reference))
+        return (AbstractVMObject*)FLIP_NMT_VALUE(reference);
+    else
+        return (AbstractVMObject*)reference;
+}
+
+inline AbstractVMObject* ReadBarrier(void** referenceHolder) {
+    if (*referenceHolder == nullptr)
+        return (AbstractVMObject*)nullptr;
+    //GCTrap(referenceHolder);
+    NMTTrap(referenceHolder);
+    return Untag(*referenceHolder);
+} */
+
+
+/*
+inline void GCTrap(void** referenceHolder) {
     size_t pageNumber = ((size_t)*referenceHolder - (size_t)memoryStart) / pageSize;
     Page* page = allPages->at(pageNumber);
-    if (_UNIVERSE->GetInterpreter()->GCTrapEnabled() && page->Blocked())
-        // wait till all mutators have enabled their gc-trap
-        while (......) {
-            pthread_cond_wait()
-        }
-        pthread_cond_signal();
+    if (_UNIVERSE->GetInterpreter()->GCTrapEnabled() && page->Blocked()) {
+        //and all mutators have there gc trap enabled
+        //pthread_mutex_lock(&gcTrapEnabledMutex);
+        //while (!) {
+        //    pthread_cond_wait(&gcTrapEnabledCondition, &gcTrapEnabledMutex);
+        //}
+        //pthread_mutex_unlock(&gcTrapEnabledMutex);
         *referenceHolder = page->LookupNewAddress((AbstractVMObject*)*referenceHolder);
     }
-}
+} */
 
-inline void PauselessHeap::ReadBarrier(void** referenceHolder) {
-    //assert(*referenceHolder != nullptr);
-    if (*referenceHolder == nullptr)
-        return;
-    GCTrap(referenceHolder);
-    NMTTrap(referenceHolder);
-}
+
+/*
+inline void GCTrapGC(void** referenceHolder) {
+    size_t pageNumber = ((size_t)*referenceHolder - (size_t)memoryStart) / pageSize;
+    Page* page = allPages->at(pageNumber);
+    if (page->Blocked()) {
+        *referenceHolder = page->LookupNewAddress((AbstractVMObject*)*referenceHolder);
+    }
+} */
 
 #endif
-
-
-
-
-
-
-
-
-//#define REFERENCE_NMT_VALUE(REFERENCE) ((((uintptr_t)REFERENCE & MASK_OBJECT_NMT) == 0) ? false : true)
-//#define FLIP_NMT_VALUE(REFERENCE) ((((uintptr_t)REFERENCE & MASK_OBJECT_NMT) == 0) ? ((pVMObject)((uintptr_t)REFERENCE | MASK_OBJECT_NMT)) : ((pVMObject)((uintptr_t)REFERENCE ^ MASK_OBJECT_NMT)))
