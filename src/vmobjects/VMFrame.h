78e71f22cf85e77ff16fd347f3df9185d7d9b6ba
Stefan Marr <git@stefan-marr.de>
2015-01-03 16:18:27 +0100

    Added helpers with store_ptr in VMFrame
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/vmobjects/VMFrame.h
+++ src/vmobjects/VMFrame.h
@@ -78,6 +78,9 @@ private:
     gc_oop_t* arguments;
     gc_oop_t* locals;
     gc_oop_t* stack_ptr;
+    
+    inline void SetLocal(long, vm_oop_t);
+    inline void SetArgument(long index, vm_oop_t value);
 
     static const long VMFrameNumberOfFields;
 };
@@ -128,4 +131,12 @@ void VMFrame::ClearPreviousFrame() {
 
 VMMethod* VMFrame::GetMethod() const {
     return load_ptr(method);
+}
+
+void VMFrame::SetLocal(long index, vm_oop_t value) {
+    store_ptr(locals[index], value);
+}
+
+void VMFrame::SetArgument(long index, vm_oop_t value) {
+    store_ptr(arguments[index], value);
 }
