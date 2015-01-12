d697ec5deface27c8c8a10dfec5f7d674a258c45
Stefan Marr <git@stefan-marr.de>
2015-01-12 13:44:10 +0100

    Use paging in copying heap
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/memory/CopyingHeap.cpp
+++ src/memory/CopyingHeap.cpp
@@ -1,43 +1,119 @@
 #include "CopyingHeap.h"
 #include "CopyingCollector.h"
 
-#include "../vmobjects/AbstractObject.h"
-#include "../vm/Universe.h"
+#include <interpreter/Interpreter.h>
+#include <vm/Universe.h>
+#include <vmobjects/AbstractObject.h>
+#include <vmobjects/VMObject.h>
 
-CopyingHeap::CopyingHeap(long objectSpaceSize) : Heap<CopyingHeap>(new CopyingCollector(this), objectSpaceSize) {
-    size_t bufSize = objectSpaceSize;
-    currentBuffer = malloc(bufSize);
-    oldBuffer = malloc(bufSize);
-    memset(currentBuffer, 0x0, bufSize);
-    memset(oldBuffer, 0x0, bufSize);
-    currentBufferEnd = (void*)((size_t)currentBuffer + bufSize);
-    collectionLimit = (void*)((size_t)currentBuffer + ((size_t)(bufSize *
-                            0.9)));
-    nextFreePosition = currentBuffer;
+
+CopyingHeap::CopyingHeap(size_t pageSize, size_t maxHeapSize)
+    : Heap<CopyingHeap>(new CopyingCollector(this)),
+      pageSize(pageSize), maxNumPages(maxHeapSize / pageSize),
+      currentNumPages(0) {}
+
+Page* CopyingHeap::RegisterThread() {
+    lock_guard<mutex> lock(pages_mutex);
+    return reinterpret_cast<Page*>(getNextPage_alreadyLocked());
 }
 
-void CopyingHeap::switchBuffers() {
-    size_t bufSize = (size_t)currentBufferEnd - (size_t)currentBuffer;
-    void* tmp = oldBuffer;
-    oldBuffer = currentBuffer;
-    currentBuffer = tmp;
-    currentBufferEnd = (void*)((size_t)currentBuffer + bufSize);
-    nextFreePosition = currentBuffer;
-    collectionLimit = (void*)((size_t)currentBuffer + (size_t)(0.9 *
-                    bufSize));
+void CopyingHeap::UnregisterThread(Page* page) {
+    // NOOP, because we the page is still in use
 }
 
-AbstractVMObject* CopyingHeap::AllocateObject(size_t size) {
-    lock_guard<mutex> lock(allocation_mutex);
-    
-    AbstractVMObject* newObject = (AbstractVMObject*) nextFreePosition;
-    nextFreePosition = (void*)((size_t)nextFreePosition + size);
-    if (nextFreePosition > currentBufferEnd) {
-        Universe::ErrorPrint("Failed to allocate " + to_string(size) + " Bytes.\n");
-        GetUniverse()->Quit(-1);
+MemoryPage<CopyingHeap>* CopyingHeap::getNextPage_alreadyLocked() {
+    MemoryPage<CopyingHeap>* result;
+
+    if (freePages.empty()) {
+        currentNumPages++;
+        if (currentNumPages > maxNumPages) {
+            // during the copy phase, we need twice as many pages, i.e.,
+            // a heap twice as large. This is similar to the classic
+            // semi-space copy-collector design. Except, that we allow
+            // too much allocation.
+            if (currentNumPages > maxNumPages * 2) {
+                ReachedMaxNumberOfPages(); // won't return
+                return nullptr;            // is not executed!
+            }
+        }
+        
+        result = new CopyingPage(this);
+    } else {
+        result = freePages.back();
+        freePages.pop_back();
     }
-    //let's see if we have to trigger the GC
-    if (nextFreePosition > collectionLimit)
+    
+    usedPages.push_back(result);
+    
+    // let's see if we have to trigger the GC
+    if (usedPages.size() > 0.9 * maxNumPages) {
         triggerGC();
-    return newObject;
+    }
+    
+    return result;
 }
+
+bool MemoryPage<CopyingHeap>::isFull() {
+    return ((uintptr_t) bufferEnd - (uintptr_t) nextFreePosition)
+                < 2 * sizeof(VMObject);
+}
+
+Page* MemoryPage<CopyingHeap>::GetCurrent() {
+    return interpreter->GetPage();
+}
+
+#include <vmobjects/VMBlock.h>
+
+void MemoryPage<CopyingHeap>::WalkObjects(walk_heap_fn walk,
+                                          Page* target) {
+    AbstractVMObject* curObject = static_cast<AbstractVMObject*>(buffer);
+    
+    while (curObject < nextFreePosition) {
+        if (dynamic_cast<VMBlock*>(curObject)) {
+            int i = 0;
+        }
+        curObject->WalkObjects(walk, target);
+        curObject = reinterpret_cast<AbstractVMObject*>(
+                        (uintptr_t)curObject + curObject->GetObjectSize());
+    }
+}
+
+void* MemoryPage<CopyingHeap>::allocateInNextPage(size_t size ALLOC_OUTSIDE_NURSERY_DECL) {
+    assert(interpreter);
+    
+    if (next == nullptr) {
+        next = heap->getNextPage();
+        next->SetInterpreter(interpreter);
+        
+        // need to set the page unconditionally, even if it is not yet
+        // completely full. Otherwise, we can have the situation that during
+        // the copying phase, an object gets moved to a previous page, and
+        // is missed while moving all dependent objects, and thus, not moving
+        // its dependent objects.
+        interpreter->SetPage(reinterpret_cast<Page*>(next));
+    }
+    
+    return next->AllocateObject(size ALLOC_HINT);
+}
+
+static gc_oop_t invalidate_objects(gc_oop_t oop, Page*) {
+    if (IS_TAGGED(oop)) {
+        return oop;
+    }
+    
+    AbstractVMObject* obj = AS_OBJ(oop);
+    obj->MarkObjectAsInvalid();
+    return oop;
+}
+
+void MemoryPage<CopyingHeap>::Reset() {
+    next             = nullptr;
+    interpreter      = nullptr;
+    nextFreePosition = buffer;
+    
+    if (DEBUG) {
+//        memset(buffer, 0, heap->pageSize);
+        WalkObjects(invalidate_objects, nullptr);
+    }
+}
+
--- src/memory/CopyingHeap.h
+++ src/memory/CopyingHeap.h
@@ -1,25 +1,104 @@
 #pragma once
 
-#include "Heap.h"
 #include <string.h>
 #include <mutex>
+#include <unordered_set>
 
+#include "Heap.h"
 
 using namespace std;
 
+class CopyingHeap;
+typedef MemoryPage<CopyingHeap> CopyingPage;
+
 class CopyingHeap : public Heap<CopyingHeap> {
-    friend class CopyingCollector;
+
 public:
     CopyingHeap(size_t pageSize, size_t maxHeapSize);
+    
     AbstractVMObject* AllocateObject(size_t size);
+    
+    Page* RegisterThread();
+    void UnregisterThread(Page*);
+
 private:
-    void switchBuffers(void);
+    const size_t pageSize;
+    const size_t maxNumPages;
+    size_t currentNumPages;
     
-    void* currentBuffer;
-    void* collectionLimit;
-    void* oldBuffer;
-    void* currentBufferEnd;
-    void* nextFreePosition;
+    mutex pages_mutex;
+    vector<CopyingPage*> usedPages;
+    vector<CopyingPage*> freePages;
     
-    mutex allocation_mutex;
+    
+    CopyingPage* getNextPage() {
+        lock_guard<mutex> lock(pages_mutex);
+        return getNextPage_alreadyLocked();
+    }
+    CopyingPage* getNextPage_alreadyLocked();
+
+    friend class CopyingCollector;
+    friend CopyingPage;
+};
+
+class Interpreter;
+
+template<>
+class MemoryPage<CopyingHeap> {
+public:
+    MemoryPage<CopyingHeap>(CopyingHeap* heap)
+        : heap(heap), interpreter(nullptr), next(nullptr),
+          buffer(malloc(heap->pageSize)),
+          bufferEnd((void*)((uintptr_t)buffer + heap->pageSize)),
+          nextFreePosition(buffer) {
+        memset(buffer, 0, heap->pageSize);
+    }
+    
+    ~MemoryPage<CopyingHeap>() {
+        free(buffer);
+    }
+    
+    void* AllocateObject(size_t size ALLOC_OUTSIDE_NURSERY_DECL) {
+        void* newObject = nextFreePosition;
+        void* newFreePosition = (void*)((uintptr_t)nextFreePosition + size);
+        
+        if (newFreePosition > bufferEnd) {
+            return allocateInNextPage(size ALLOC_HINT);
+        }
+
+        nextFreePosition = newFreePosition;
+        return newObject;
+    }
+
+    // return the current page to be used for allocation
+    // this is managed by the interpreter associated with a page
+    // when a page fills up, the interpreter can request a new page
+    // we stop using the old page when it is full
+    // and full means, something like failing
+    // on an allocation reqest for an object that
+    // is smaller than 2*sizeof(VMObject)
+
+    Page* GetCurrent();
+    void SetInterpreter(Interpreter* interp) { interpreter = interp; }
+    
+    void WalkObjects(walk_heap_fn, Page* target);
+
+    void Reset();
+    
+private:
+
+    CopyingHeap* const heap;
+    Interpreter*       interpreter;
+    CopyingPage*       next;
+
+    void* const        buffer;
+    void* const        bufferEnd;
+
+    void*              nextFreePosition;
+
+    bool isFull();
+    void* allocateInNextPage(size_t size ALLOC_OUTSIDE_NURSERY_DECL);
+    
+    friend class CopyingHeap;
+    friend class CopyingCollector;
 };
