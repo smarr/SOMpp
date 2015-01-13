be863a14a16b07b61a59b0133d0d8c934a2c3557
Stefan Marr <git@stefan-marr.de>
2015-01-13 23:43:13 +0100

    Added missing includes
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/memory/GenerationalHeap.h
+++ src/memory/GenerationalHeap.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <assert.h>
+#include <string.h>
 
 #include <misc/defs.h>
 
--- src/memory/MarkSweepHeap.h
+++ src/memory/MarkSweepHeap.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "../misc/defs.h"
+#include <string.h>
+#include <misc/defs.h>
 
 #include "Heap.h"
 #include "Page.h"
