69e5eda21a5fc43d4aecf587a2a5fab5fa6cf26b
Stefan Marr <git@stefan-marr.de>
2015-01-16 00:01:30 +0100

    Add tagging support to Delay.cpp
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/primitives/Delay.cpp
+++ src/primitives/Delay.cpp
@@ -16,16 +16,15 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void _Delay::Wait(pVMObject object, pVMFrame frame){
-    pVMObject self = frame->Pop();
-    pVMInteger integer = (pVMInteger)(static_cast<VMObject*>(self)->GetField(0));
-    //cout << "DELAY : thread sleeping, threadID: " << _UNIVERSE->GetInterpreter()->GetThread()->GetThreadId() << "needs to wait: " << integer->GetEmbeddedInteger()*1000 << endl;
-    usleep(integer->GetEmbeddedInteger()*1000);
-    //cout << "DELAY : thread has woken up, threadID: " << _UNIVERSE->GetInterpreter()->GetThread()->GetThreadId() << endl;
+void _Delay::Wait(VMObject* object, VMFrame* frame){
+    VMObject* self = static_cast<VMObject*>(frame->Pop());
+    int64_t delay = INT_VAL(self->GetField(0));
+
+    usleep((useconds_t) delay * 1000);
+
     frame->Push(self);
 }
 
 _Delay::_Delay() : PrimitiveContainer() {
-    this->SetPrimitive("wait", new
-                       Routine<_Delay>(this, &_Delay::Wait));
+    SetPrimitive("wait", new Routine<_Delay>(this, &_Delay::Wait));
 }
