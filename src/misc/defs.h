767b546df028e79c6e003ae17e468bb1b3200b9c
Stefan Marr <git@stefan-marr.de>
2015-01-20 19:08:07 +0100

    Added FORCE_INLINE macro
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/misc/defs.h
+++ src/misc/defs.h
@@ -145,6 +145,8 @@
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
 
+#define FORCE_INLINE __attribute__ ((always_inline)) inline
+
 
 typedef std::string StdString;
 
