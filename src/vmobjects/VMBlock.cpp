2dccb1bdaa9d28e68a08a0a3142604e97969554d
Stefan Marr <git@stefan-marr.de>
2015-02-10 15:48:27 +0100

    VMBlock shouldn't have any additional fields
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/vmobjects/VMBlock.cpp
+++ src/vmobjects/VMBlock.cpp
@@ -38,7 +38,7 @@ void VMBlock::SetMethod(VMMethod* bMethod) {
 }
 
 VMBlock* VMBlock::Clone(Page* page) {
-    VMBlock* clone = new (page, GetAdditionalSpaceConsumption() ALLOC_MATURE) VMBlock(*this);
+    VMBlock* clone = new (page, 0 ALLOC_MATURE) VMBlock(*this);
     return clone;
 }
 
