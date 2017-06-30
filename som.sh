995731a613c961855ed27b65c63793ea68f547b4
Stefan Marr <git@stefan-marr.de>
2017-07-01 00:18:12 +0200

    Make launcher script location independent
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd`
+popd > /dev/null
+
 if [ "`uname`" = "Linux" ]
 then
-    export LD_LIBRARY_PATH=.
+    export LD_LIBRARY_PATH=${SCRIPT_PATH}/
 fi
-./SOM++ "$@"
+${SCRIPT_PATH}/SOM++ "$@"
