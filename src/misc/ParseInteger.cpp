c9a1c1a45a978605ea26cb91d0f3dbb8d28cc514
Stefan Marr <git@stefan-marr.de>
2023-01-22 23:15:10 +0000

    Update Xcode project
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/misc/ParseInteger.cpp
@@ -0,0 +1,8 @@
+//
+//  ParseInteger.cpp
+//  SOM
+//
+//  Created by Stefan Marr on 22/01/2023.
+//
+
+#include <stdio.h>
---
+++ src/misc/ParseInteger.h
@@ -0,0 +1,12 @@
+//
+//  ParseInteger.h
+//  SOM
+//
+//  Created by Stefan Marr on 22/01/2023.
+//
+
+#ifndef ParseInteger_h
+#define ParseInteger_h
+
+
+#endif /* ParseInteger_h */
