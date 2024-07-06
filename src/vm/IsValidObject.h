6f7cca0c36998913715a7382bcf760b17d49b15a
Stefan Marr <git@stefan-marr.de>
2024-07-06 16:59:12 +0100

    Refactor IsValidObject to avoid link issues in Xcode for profiling
    This means IsValidObject() isn’t an inlined method in debugging mode though. Can fix that later.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/vm/IsValidObject.h
+++ src/vm/IsValidObject.h
@@ -3,21 +3,9 @@
 #include <misc/defs.h>
 #include <vmobjects/ObjectFormats.h>
 
-#if DEBUG
+bool IsValidObject(vm_oop_t obj);
 
-    bool IsValidObject(vm_oop_t obj);
+void set_vt_to_null();
 
-    void set_vt_to_null();
+void obtain_vtables_of_known_classes(VMSymbol* className);
 
-    void obtain_vtables_of_known_classes(VMSymbol* className);
-#else
-
-    void set_vt_to_null() {}
-
-    void obtain_vtables_of_known_classes(VMSymbol* className) {}
-
-    inline bool IsValidObject(vm_oop_t obj) {
-        return true;
-    }
-
-#endif
