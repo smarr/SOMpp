ad94568eb8c6c4df77606555e61530802853e639
Stefan Marr <git@stefan-marr.de>
2024-08-15 23:40:07 +0200

    Run format after lint again
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- lint.sh
+++ lint.sh
@@ -4,3 +4,4 @@ clang-tidy-mp-18 --config-file=.clang-tidy src/**/*.cpp \
   --fix \
   -- -I/opt/local/include/ -fdiagnostics-absolute-paths \
   -DGC_TYPE=GENERATIONAL -DUSE_TAGGING=false -DCACHE_INTEGER=false -DUNITTESTS
+clang-format-mp-18 -i --style=file --Werror src/*.cpp  src/**/*.cpp src/**/*.h
