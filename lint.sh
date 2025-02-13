6b7ff6cd7956622ae9d9dfefd2a36cfec96a9905
Stefan Marr <git@stefan-marr.de>
2025-02-13 22:46:35 +0000

    Use Clang 19 for linting
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-clang-format-mp-18 -i --style=file --Werror src/*.cpp  src/**/*.cpp src/**/*.h
-clang-tidy-mp-18 --config-file=.clang-tidy src/**/*.cpp \
+clang-format-mp-19 -i --style=file --Werror src/*.cpp  src/**/*.cpp src/**/*.h
+clang-tidy-mp-19 --config-file=.clang-tidy src/**/*.cpp \
   --fix \
   -- -I/opt/local/include/ -fdiagnostics-absolute-paths \
   -DGC_TYPE=GENERATIONAL -DUSE_TAGGING=false -DCACHE_INTEGER=false -DUNITTESTS
-clang-format-mp-18 -i --style=file --Werror src/*.cpp  src/**/*.cpp src/**/*.h
+clang-format-mp-19 -i --style=file --Werror src/*.cpp  src/**/*.cpp src/**/*.h
