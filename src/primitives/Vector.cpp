703a7b65ab75df3ac91448e49988e436b6990873
Rhys Walker <rhys.h.walker@gmail.com>
2025-05-22 12:19:49 +0100

    Implement more Vector methods as primitives
    - #at:put:
    - #append:
    - #first
    - #last

--- src/primitives/Vector.cpp
+++ src/primitives/Vector.cpp
@@ -18,7 +18,35 @@ static vm_oop_t vecAt(vm_oop_t obj, vm_oop_t arg) {
     return self->GetIndexableField(index);
 }
 
+static vm_oop_t vecAtPut(vm_oop_t obj, vm_oop_t at, vm_oop_t put) {
+    auto* self = static_cast<VMVector*>(obj); // Cast itself as a VMVector
+    int64_t const index = INT_VAL(at); // Set the index looking for
+    // Call method to set the value at index. That deals with 1to0 indexing conversion
+    self->SetIndexableField(index, put);
+    return put; // Return the value that was set
+}
+
+static vm_oop_t vecAppend(vm_oop_t obj, vm_oop_t arg) {
+    auto* self = static_cast<VMVector*>(obj);
+    self->Append(arg);
+    return self;
+}
+
+static vm_oop_t vecFirst(vm_oop_t obj) {
+    auto* self = static_cast<VMVector*>(obj);
+    return self->GetFirst();
+}
+
+static vm_oop_t vecLast(vm_oop_t obj) {
+    auto* self = static_cast<VMVector*>(obj);
+    return self->GetLast();
+}
+
 _Vector::_Vector() {
     Add("new:", &vecNew, true);
     Add("at:", &vecAt, false);
+    Add("at:put:", &vecAtPut, false);
+    Add("append:", &vecAppend, false);
+    Add("first", &vecFirst, false);
+    Add("last", &vecLast, false);
 }
--- src/vmobjects/VMVector.h
+++ src/vmobjects/VMVector.h
@@ -4,7 +4,9 @@
 
 #include "../vmobjects/VMInteger.h"
 #include "../vmobjects/VMObject.h"
+#include "../vm/Universe.h"
 #include "VMArray.h"
+#include "ObjectFormats.h"
 
 class VMVector : public VMObject {
 public:
@@ -16,6 +18,7 @@ public:
         int64_t const first = INT_VAL(load_ptr(this->first));
         int64_t const last = INT_VAL(load_ptr(this->last));
         VMArray* const storage = load_ptr(this->storage);
+
         if (index < 1 || index > first + last) {
             IndexOutOfBounds(index, (last - first));
             // TODO(smarr): check if this would be correct
@@ -23,6 +26,53 @@ public:
         vm_oop_t returned = storage->GetIndexableField(first + index - 2);
         return returned;
     }
+
+    [[nodiscard]] inline vm_oop_t GetFirst() {
+        int64_t first = INT_VAL(load_ptr(this->first));
+        vm_oop_t returned = GetIndexableField(1);
+        return returned;
+    }
+
+    [[nodiscard]] inline vm_oop_t GetLast() {
+        int64_t last = INT_VAL(load_ptr(this->last));
+        int64_t first = INT_VAL(load_ptr(this->first));
+        vm_oop_t returned = GetIndexableField(last-1);
+        return returned;
+    }
+
+    inline void SetIndexableField(size_t index, vm_oop_t value) {
+        int64_t const first = INT_VAL(load_ptr(this->first));
+        int64_t const last = INT_VAL(load_ptr(this->last));
+        VMArray* const storage = load_ptr(this->storage);
+        if (index < 1 || index > first + last) {
+            IndexOutOfBounds(index, (last - first));
+            // TODO(smarr): check if this would be correct
+        }
+        storage->SetIndexableField(first + index - 2, value);
+    }
+
+    inline void Append(vm_oop_t value) {
+        int64_t first = INT_VAL(load_ptr(this->first));
+        int64_t last = INT_VAL(load_ptr(this->last));
+        VMArray* storage = load_ptr(this->storage);
+
+        if (last >= storage->GetNumberOfIndexableFields()) { // Expand the array
+            // Expand by the correct amount *2 by default in the native som implementation
+            VMArray * newStorage = Universe::NewArray(storage->GetNumberOfIndexableFields()*2);
+
+            storage->CopyIndexableFieldsTo(newStorage);
+            newStorage->SetIndexableField(last-1, value);
+            
+            SetField(2 /* storage */, newStorage);
+
+        } else { // Just set the new value
+            storage->SetIndexableField(last-1, value);
+        }
+
+        last += 1;
+        this->last = store_ptr(this->last, NEW_INT(last));
+    }
+
 
     static __attribute__((noreturn)) __attribute__((noinline)) void
     IndexOutOfBounds(size_t idx, size_t size);
